6b3bafef3825803bac05908fb37057624a11374e
shoppie70 <sho.tsuka.0709@gmail.com>
2025-04-05 08:26:21 +0000

    レスポンシブ状態での見た目調整

--- public/build/assets/Edit-BvgWMAD8.js
+++ public/build/assets/Edit-BvgWMAD8.js
@@ -1,1 +1,48 @@
-import{j as s,L as m}from"./app-CZKEO1x-.js";import{A as r}from"./AuthenticatedLayout-DKgn2jJ9.js";import t from"./DeleteUserForm-CNdWN3iQ.js";import i from"./UpdatePasswordForm-DRtX8ZxL.js";import o from"./UpdateProfileInformationForm-CklS8Qhi.js";import"./transition-Y6kxkuTI.js";import"./TextInput-DDmsVGvq.js";import"./InputLabel-Tzh6lzrR.js";import"./PrimaryButton-BgWkAW4R.js";function w({mustVerifyEmail:e,status:a}){return s.jsxs(r,{header:s.jsx("h2",{className:"text-xl font-semibold leading-tight text-gray-800",children:"Profile"}),children:[s.jsx(m,{title:"Profile"}),s.jsx("div",{className:"py-12",children:s.jsxs("div",{className:"mx-auto max-w-7xl space-y-6 sm:px-6 lg:px-8",children:[s.jsx("div",{className:"bg-white p-4 shadow sm:rounded-lg sm:p-8",children:s.jsx(o,{mustVerifyEmail:e,status:a,className:"max-w-xl"})}),s.jsx("div",{className:"bg-white p-4 shadow sm:rounded-lg sm:p-8",children:s.jsx(i,{className:"max-w-xl"})}),s.jsx("div",{className:"bg-white p-4 shadow sm:rounded-lg sm:p-8",children:s.jsx(t,{className:"max-w-xl"})})]})})]})}export{w as default};
+import { j as s, L as m } from './app-CZKEO1x-.js';
+import { A as r } from './AuthenticatedLayout-DKgn2jJ9.js';
+import t from './DeleteUserForm-CNdWN3iQ.js';
+import i from './UpdatePasswordForm-DRtX8ZxL.js';
+import o from './UpdateProfileInformationForm-CklS8Qhi.js';
+import './transition-Y6kxkuTI.js';
+import './TextInput-DDmsVGvq.js';
+import './InputLabel-Tzh6lzrR.js';
+import './PrimaryButton-BgWkAW4R.js';
+function w({ mustVerifyEmail: e, status: a }) {
+    return s.jsxs(r, {
+        header: s.jsx('h2', {
+            className: 'text-xl font-semibold leading-tight text-gray-800',
+            children: 'Profile',
+        }),
+        children: [
+            s.jsx(m, { title: 'Profile' }),
+            s.jsx('div', {
+                className: 'py-12',
+                children: s.jsxs('div', {
+                    className: 'mx-auto max-w-7xl space-y-6 sm:px-6 lg:px-8',
+                    children: [
+                        s.jsx('div', {
+                            className:
+                                'bg-white p-4 shadow sm:rounded-lg sm:p-8',
+                            children: s.jsx(o, {
+                                mustVerifyEmail: e,
+                                status: a,
+                                className: 'max-w-xl',
+                            }),
+                        }),
+                        s.jsx('div', {
+                            className:
+                                'bg-white p-4 shadow sm:rounded-lg sm:p-8',
+                            children: s.jsx(i, { className: 'max-w-xl' }),
+                        }),
+                        s.jsx('div', {
+                            className:
+                                'bg-white p-4 shadow sm:rounded-lg sm:p-8',
+                            children: s.jsx(t, { className: 'max-w-xl' }),
+                        }),
+                    ],
+                }),
+            }),
+        ],
+    });
+}
+export { w as default };
--- public/build/assets/PrimaryButton-BgWkAW4R.js
+++ public/build/assets/PrimaryButton-BgWkAW4R.js
@@ -1,1 +1,12 @@
-import{j as n}from"./app-CZKEO1x-.js";function s({className:r="",disabled:t,children:e,...o}){return n.jsx("button",{...o,className:`inline-flex items-center rounded-md border border-transparent bg-orange-500 px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-gray-700 focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 active:bg-gray-900 ${t&&"opacity-25"} `+r,disabled:t,children:e})}export{s as P};
+import { j as n } from './app-CZKEO1x-.js';
+function s({ className: r = '', disabled: t, children: e, ...o }) {
+    return n.jsx('button', {
+        ...o,
+        className:
+            `inline-flex items-center rounded-md border border-transparent bg-orange-500 px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-gray-700 focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 active:bg-gray-900 ${t && 'opacity-25'} ` +
+            r,
+        disabled: t,
+        children: e,
+    });
+}
+export { s as P };
